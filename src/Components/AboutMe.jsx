c29f55a73f0813a16b2213cb651d24d4336538b4
baehaky <baehaky1504@gmail.com>
2024-01-03 21:02:39 +0700

    modified:   src/Components/AboutMe.jsx

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -6,13 +6,12 @@ export function AboutMe() {
   return (
     <div
       id="aboutme"
-      className="flex flex-col min-h-screen lg:flex-row justify-center items-center"
+      className="min-h-screen flex flex-col lg:flex-row justify-center items-center"
     >
       <motion.CardHeader
-        animate={{ y: 50 }}
         initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ ease: "easeOut", duration: 2 }}
+        whileInView={{ opacity: 1}}
+        transition={{ ease: "easeIn", duration: 3 }}
         shadow={true}
         floated={false}
         className="rounded-full w-[50%] sm:w-[25%] lg:w-[20%] animate-border bg-white bg-gradient-to-r from-cyan-500 via-light-blue-800 to-indigo-800 bg-[length:400%_400%] flex justify-center items-center p-1"
@@ -24,20 +23,23 @@ export function AboutMe() {
         />
       </motion.CardHeader>
       <motion.cardBody
-        animate={{ y: 100 }}
-        className="mt-5"
+        animate={{ y: 200 }}
+        className="mt-5 w-[90%] sm:w-[80%] lg:w-[50%]"
         initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, y:0}}
-        transition={{ ease: "easeOut", duration: 2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeIn", duration: 2 }}
       >
         <Typography
           variant="h4"
           color="blue-gray"
-          className="mb-2 underline font-bold text-center lg:text-left lg:mx-5"
+          className="mb-2 underline font-bold text-center mx-7 lg:text-left"
         >
           Muhamad Baehaky
         </Typography>
-        <Typography color="gray" className="mb-4 font-normal mx-5 text-left lg:text-left sm:text-center">
+        <Typography
+          color="gray"
+          className="mb-4 font-normal w-[90%] lg:w-[90%] text-left lg:text-left mx-7 sm:text-center"
+        >
           I am an Information System student at Multimedia Nusantara University,
           actively combining academic learning with practical expertise.
           Certified in Python programming and data visualization, I excel in
@@ -46,7 +48,7 @@ export function AboutMe() {
           dynamic skill set and ongoing education to contribute effectively to
           innovative projects.
         </Typography>
-        <div className="flex flex-wrap gap-2 mx-5">
+        <div className="flex flex-wrap w-[90%] gap-2 mx-7">
           <span className="animate-border inline-block rounded-full bg-gradient-to-r from-cyan-500 via-light-blue-800 to-indigo-800 bg-[length:400%_400%] p-1">
             <span className="block rounded-full bg-slate-900 px-5 py-2 font-bold text-black bg-white">
               HTML
